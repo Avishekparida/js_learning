5e4d45196a6bfcd02741e3f62b6fe71ab619ee14
Avishekparida <109265341+Avishekparida@users.noreply.github.com>
2024-06-22 08:06:35 +0000

    container config done

---
+++ .devcontainer/Bsics_javascript/test.js
@@ -0,0 +1,13 @@
+console.log("Avishek")
+
+let a =7
+for(let c=1;c<10;c++){
+    a=a+c;
+    console.log(a);
+    console.log(typeof(a))
+}
+
+var b={"a":"Ram","b":"Hari"}
+console.log(b.a+9)
+let c = "Hari"
+console.log(typeof(c))
